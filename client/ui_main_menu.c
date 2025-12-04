e5305a6ea18e3a28abb399aa5447fe11e053cd5f
lia0519 <lita050519@naver.com>
2025-12-04 17:25:22 +0900

    test

--- client/ui_main_menu.c
+++ client/ui_main_menu.c
@@ -1,6 +1,8 @@
 #include "ui_main_menu.h"
 
 void ui_main_menu_show() {
+    printf("Welcome to the Bulletin Board System!\n");
+
     printf("\n=== 메인 메뉴 ===\n");
     printf("1. 로그인\n");
     printf("2. 회원가입\n");
